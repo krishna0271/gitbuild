4c17e244d7756feaa5c9a22b16795ce6feed567a
krishna0271 <krrishna9848@gmail.com>
2018-12-28 15:50:20 +0530

    commit by author 1

---
+++ DUMMY/src/COM/DUMMY/App.java
@@ -0,0 +1,13 @@
+package COM.DUMMY;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
